dddcc6d4ac926d391a5f55f750a57c4b6f861120
tracyc <49257820+tracychew@users.noreply.github.com>
2020-07-22 11:35:54 +1000

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -7,4 +7,4 @@ NCPUS=`echo $1 | cut -d ',' -f 4`
 
 echo "$(date): Running fastQC. Fastq:${fastq}, Output:${out}, Log file: ${logfile}, NCPUS:${NCPUS}" >> ${logfile} 2>&1 
 
-fastqc -t ${NCPUS} --extract ${fastq} -o ${out} ${fastq} >> ${logfile} 2>&1 
+fastqc -t ${NCPUS} --extract -o ${out} ${fastq} >> ${logfile} 2>&1 
